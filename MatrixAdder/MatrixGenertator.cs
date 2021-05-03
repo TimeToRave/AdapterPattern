24259e4955fd8c4f6d57d25de25172f457372dc5
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-03 22:29:32 +0300

    Добавлен класс-программа генерации матриц Программа генерирует две матрицы и записывает их в один файл F3

---
+++ MatrixAdder/MatrixGenertator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MatrixAdder
+{
+    public class MatrixGenerator
+    {
+        public void WriteRandomMatricesToFile(int sizeX, int sizeY)
+        {
+            var matrixString = $"{GenerateMatrix(sizeX, sizeY)}#{GenerateMatrix(sizeX, sizeY)}";
+            
+            FileOperator fileOperator = new FileOperator();
+            fileOperator.WriteTextToFile("F3.txt", matrixString);
+        }
+        
+        private Matrix GenerateMatrix(int sizeX, int sizeY) 
+        {
+            Random random = new Random();
+            int[,] result = new int[sizeX, sizeY];
+            
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    result[i, j] = random.Next(-1000, 1000);
+                }
+            }
+
+            return new Matrix(result);
+        }
+    }
+}
