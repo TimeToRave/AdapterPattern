4bf3de363627e4b788dd8155830329cbcbb6d281
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-03 22:29:03 +0300

    Реализован класс-программа для сложения матриц
    Программа читает данные из файла F0, парсит его содержимое как матрицы и их скалдывает. Результат записывается в файл F1

---
+++ MatrixAdder/MatrixAdder.cs
@@ -0,0 +1,24 @@
+﻿namespace MatrixAdder
+{
+    public class MatrixAdder
+    {
+        public void SumMatrix()
+        {
+            // читает две матрицы из файла
+            FileOperator fileOperator = new FileOperator();
+            string fileContent = fileOperator.ReadTextFromFile("F0.txt");
+            fileContent = fileContent.Replace("\r\n", string.Empty);
+            string[] textMatrix = fileContent.Split('#');
+
+            if (textMatrix.Length > 1)
+            {
+                var source = new Matrix(textMatrix[0]);
+                var addendum = new Matrix(textMatrix[1]);
+
+                var result = source + addendum;
+
+                fileOperator.WriteTextToFile("F1.txt", result.ToString());
+            }
+        }
+    }
+}
