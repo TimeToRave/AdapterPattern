3c0accf8df10e16ef280aafbd019d13081e79fce
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-03 22:29:45 +0300

    Класс Program

---
+++ MatrixAdder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MatrixAdder
+{
+    class Program
+    {
+        static void Main()
+        {
+            var adder = new MatrixAdder();
+            adder.SumMatrix();
+
+            var generator = new MatrixGenerator();
+            generator.WriteRandomMatricesToFile(4, 6);
+        }
+    }
+}
