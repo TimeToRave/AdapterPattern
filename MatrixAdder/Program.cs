b53ef7930a60e3488ffa6828415c1ae52b3ec58a
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-04 21:49:04 +0300

    Демаонстрация работы класса адаптера

--- MatrixAdder/Program.cs
+++ MatrixAdder/Program.cs
@@ -1,18 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace MatrixAdder
+﻿namespace MatrixAdder
 {
     class Program
     {
         static void Main()
         {
-            var adder = new MatrixAdder();
-            adder.SumMatrix();
-
-            var generator = new MatrixGenerator();
-            generator.RunOperation(4, 6);
+            OperateMatrix(new MatrixGenerator(), 4, 6);
+            OperateMatrix(new MatricesAdderAdapter(), 4, 6);
+        }
+        
+        /// <summary>
+        /// Выполняет операции над матрицами
+        /// </summary>
+        /// <param name="matricesHandler">Экземпляр класса работы с матрицами</param>
+        /// <param name="sizeX">Количество строк матриц</param>
+        /// <param name="sizeY">Количество столбцов матриц</param>
+        private static void OperateMatrix(IMatricesHandler matricesHandler, int sizeX, int sizeY)
+        {
+            Matrix[] matrices = matricesHandler.PrepareMatrices(sizeX, sizeY);
+            matrices = matricesHandler.OperateMatrices(matrices);
+            matricesHandler.SaveMatrices(matrices);
         }
     }
 }
